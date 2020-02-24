9dd8b114cb1c553864b242d989a9d8f3c7c2ee53
Peter Stevens <Peterstevens510@gmail.com>
2020-02-24 15:24:52 +0000

    added Default limit constant with string interpolation

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,12 @@
 class Oystercard
-  attr_reader :balance
-
+  attr_reader :balance 
+DEFAULT_LIMIT = 90
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Balance cannot exceed #{DEFAULT_LIMIT}" if @balance + amount > DEFAULT_LIMIT
   @balance += amount
   end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'Oystercard.rb'
 
 describe Oystercard do
+  let(:oystercard) { Oystercard.new }
   it 'Oystercard has default balance of zero' do
     expect(subject.balance).to eq(0)
   end
@@ -9,6 +10,11 @@ it 'top_up method which adds money to my Oysercard balance' do
   expect(subject.top_up(5)).to eq(5)
 end
 
+it 'top_up method rasies error if it will bring balance above £90' do 
+  oystercard.top_up(Oystercard::DEFAULT_LIMIT)
+  expect { oystercard.top_up(1) }.to raise_error "Balance cannot exceed #{Oystercard::DEFAULT_LIMIT}"
 end
 
+
+end
 
