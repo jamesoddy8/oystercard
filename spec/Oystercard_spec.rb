87d3980057b5a118ddf79ecb95ed2c8f72ccc29b
Peter Stevens <Peterstevens510@gmail.com>
2020-02-24 15:01:34 +0000

    updated Oysercard file with a top_up method passing in amount as a parameter

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -4,4 +4,11 @@ describe Oystercard do
   it 'Oystercard has default balance of zero' do
     expect(subject.balance).to eq(0)
   end
+
+it 'top_up method which adds money to my Oysercard balance' do
+  expect(subject.top_up(5)).to eq(5)
 end
+
+end
+
+
